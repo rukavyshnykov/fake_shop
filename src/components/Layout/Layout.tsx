e4d6011cf0a07eda6ff0a0bfde77f27b67f0769a
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-10-08 20:32:14 +0300

    update: added navbar

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,21 +1,38 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import cl from './Layout.module.scss'
+import logo from '../../assets/logo.png'
+import { useGetProductsQuery } from "../../services/productsApi"
 
 export const Layout = () => {
-    return (
-        <>
-            <header>
-                asd
-            </header>
-            <nav>
+    const navigate = useNavigate()
+    const {isLoading, isFetching, isError} = useGetProductsQuery()
 
+    if(isLoading || isFetching) {
+        return <>Loading...</>
+    }
+
+    if(isError) {
+        <>Oops! Something went wrong</>
+    }
+
+    return (
+        <div className={cl.root}>
+            <header>
+                <div className={cl.logo}>
+                    <img src={logo}/>
+                </div>
+            </header>
+            <nav className={cl.nav}>
+                <button type="button" onClick={() => navigate('/')}>Home</button>
+                <button type="button" onClick={() => navigate('/products')}>Products</button>
+                <button type="button" onClick={() => navigate('/cart')}>Cart</button>
             </nav>
             <main className={cl.main}>
                 <Outlet />
             </main>
-            <footer>
-                asd
+            <footer className={cl.footer}>
+                FOOTER
             </footer>
-        </>
+        </div>
     )
 }
